5596d1ba21fd4d0f777a8d7b25fd315d63676d13
Cobra 陳俊宇 <cobra20621@gmail.com>
2023-08-26 17:48:16 +0800

    feat: add Ally Template, Enemy Template

--- Assets/Scripts/Data/Characters/CharacterDataBase.cs
+++ Assets/Scripts/Data/Characters/CharacterDataBase.cs
@@ -5,13 +5,9 @@ namespace NueGames.Data.Characters
 {
     public abstract class CharacterDataBase : SerializedScriptableObject
     {
-        [Header("Base")]
-        [SerializeField] protected string characterID;
         [SerializeField] protected string characterName;
         [SerializeField] [TextArea] protected string characterDescription;
         [SerializeField] protected int maxHealth;
-
-        public string CharacterID => characterID;
 
         public string CharacterName => characterName;
 
