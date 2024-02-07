cbce72d89a18034d88e278eadd13afbbe139bc1f
linglinghsieh00 <102644365+linglinghsieh00@users.noreply.github.com>
2024-02-07 14:06:55 +0800

    renew
    test

---
+++ Assets/Scripts/Feedback/RadarTest.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class RadarTest : MonoBehaviour
+{
+    public UIPolygon uiPolygon;
+    List<float> datas = new List<float>();
+
+    void Start()
+    {
+        // N
+        datas.Add(0.92f);
+        // S
+        datas.Add(0.31f);
+        // R
+        datas.Add(0.36f);
+        // D
+        datas.Add(0.28f);
+        uiPolygon.DrawPolygon(datas);
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            for(int i=0,cnt = datas.Count;i<cnt;++i)
+            {
+                datas[i] = Random.Range(0f, 1f);
+            }
+            
+            uiPolygon.DrawPolygon(datas);
+        }
+    }
+
+}
