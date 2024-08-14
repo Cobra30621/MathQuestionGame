6a87a5538282a99ed6d87e9d1cfb704485ff0429
nelsonkct <nelsonkuo0430@gmail.com>
2024-08-14 22:58:58 +0800

    feat : add Card(滿等護盾with反彈)

---
+++ Assets/Scripts/Power/Buff/ThronsPower.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using Action.Parameters;
+using Combat;
+using NueGames.Action;
+using NueGames.Characters;
+using NueGames.Combat;
+using NueGames.Enums;
+using NueGames.Managers;
+using NueGames.Parameters;
+using NueGames.UI;
+using UnityEngine;
+
+namespace NueGames.Power
+{
+
+
+    /// <summary>
+    /// 反彈能力
+    /// </summary>
+    public class ThornsPower : PowerBase
+    {
+        public override PowerName PowerName => PowerName.Thorns;
+
+
+        public override void SubscribeAllEvent()
+        {
+            Owner.OnAttacked += OnAttacked;
+            CombatManager.OnTurnStart += OnTurnStart;
+        }
+
+        public override void UnSubscribeAllEvent()
+        {
+            Owner.OnAttacked -= OnAttacked;
+            CombatManager.OnTurnStart -= OnTurnStart;
+        }
+
+
+        // 怪物攻擊時，造成傷害後反彈傷害
+        protected override void OnAttacked(DamageInfo info)
+        {
+            var source = info.ActionSource.SourceCharacter;
+            // 怪物攻擊時，造成傷害後反彈傷害
+            Debug.Log(source);
+            if (source != null)
+            {
+                // 造成與層數相等的傷害
+                var damageInfo = new DamageInfo(Amount, GetActionSource(), fixDamage: true);
+                GameActionExecutor.AddAction(new DamageAction(damageInfo, new List<CharacterBase>() {info.ActionSource.SourceCharacter}));
+                Debug.Log("ThornsPower反彈傷害");
+                // 反彈後減層數 1 
+                GameActionExecutor.AddAction(
+                    new ApplyPowerAction(-1, PowerName, 
+                        new List<CharacterBase>(){Owner}, GetActionSource()));
+                Debug.Log("ThornsPower層數減少");
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Power/Buff/VulnerablePower.cs
@@ -0,0 +1,25 @@
+﻿using GameListener;
+using NueGames.Enums;
+using UnityEngine;
+
+namespace NueGames.Power
+{
+    /// <summary>
+    /// 易傷，受到的傷害變 1.5 倍
+    /// </summary>
+    public class VulnerablePower : PowerBase
+    {
+        public override PowerName PowerName => PowerName.Vulnerable;
+
+        public VulnerablePower()
+        {
+            DecreaseOverTurn = true;
+            DamageCalculateOrder = CalculateOrder.MultiplyAndDivide;
+        }
+
+        public override float AtDamageReceive(float damage)
+        {
+            return damage * 1.5f;
+        }
+    }
+}
