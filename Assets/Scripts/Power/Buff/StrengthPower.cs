62292e5bd9596069c0726b45c2cd247556e4e274
nelsonkct <nelsonkuo0430@gmail.com>
2024-08-15 14:23:30 +0800

    feat:add mage and thief cards

---
+++ Assets/Scripts/Power/Buff/StrengthPower.cs
@@ -0,0 +1,27 @@
+﻿using GameListener;
+using NueGames.Combat;
+using NueGames.Enums;
+using UnityEngine;
+
+namespace NueGames.Power
+{
+    /// <summary>
+    /// 力量
+    /// </summary>
+    public class StrengthPower : PowerBase
+    {
+        public override PowerName PowerName => PowerName.Strength;
+
+        public StrengthPower()
+        {
+            CanNegativeStack = true;
+            DamageCalculateOrder = CalculateOrder.AdditionAndSubtraction;
+        }
+        
+        
+        public override float AtDamageGive(float damage)
+        {
+            return damage + Amount;
+        }
+    }
+}
