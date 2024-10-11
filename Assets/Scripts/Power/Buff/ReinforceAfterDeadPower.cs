b49bacc3da83121c563000d8f6b4a065150d3b7b
nelsonkct <nelsonkuo0430@gmail.com>
2024-10-11 12:52:53 +0800

    feat : add ReinforceAfterDeadPower and ReinforcePower 新增樹人技能附屬Buff：死後強化 and 強化

---
+++ Assets/Scripts/Power/Buff/ReinforceAfterDeadPower.cs
@@ -0,0 +1,36 @@
+using GameListener;
+using NueGames.Combat;
+using NueGames.Enums;
+using UnityEngine;
+using System.Collections.Generic;
+using Action.Parameters;
+using Combat;
+using NueGames.Action;
+using NueGames.Characters;
+using NueGames.Combat;
+using NueGames.Managers;
+
+namespace NueGames.Power
+{
+    /// <summary>
+    /// 強化
+    /// </summary>
+    public class ReinforceAfterDeadPower : PowerBase
+    {
+        public override PowerName PowerName => PowerName.ReinforceAfterDead;
+
+        public ReinforceAfterDeadPower()
+        {
+            
+        }
+        
+        
+        protected override void OnDead(DamageInfo damageInfo)
+        {
+            // 對全體敵方單位施加強化
+            GameActionExecutor.AddAction(
+                new ApplyPowerAction(1, PowerName.Reinforce, 
+                    new List<CharacterBase>() {}, GetActionSource()));
+        }
+    }
+}
