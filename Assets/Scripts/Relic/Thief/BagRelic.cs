8433c54940d377fe3a15ef0efae48cd3caba3945
nelsonkct <nelsonkuo0430@gmail.com>
2025-01-24 20:55:26 +0800

    fix : enable bag relic

--- Assets/Scripts/Relic/Thief/BagRelic.cs
+++ Assets/Scripts/Relic/Thief/BagRelic.cs
@@ -1,4 +1,6 @@
 using Relic.Data;
+using UnityEngine;
+
 namespace Relic.Thief
 {
     public class BagRelic : RelicBase
@@ -6,6 +8,8 @@ namespace Relic.Thief
         public override RelicName RelicName => RelicName.Bag;
         public override int AtGainTurnStartDraw(int value)
         {
+            Debug.Log("BagRelic AtGainTurnStartDraw");
+            Debug.Log(value);
             return value + 1;
         }
     }
