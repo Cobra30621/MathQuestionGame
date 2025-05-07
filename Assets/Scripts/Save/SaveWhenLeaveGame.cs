31abfbfc39cea3487dda609174ad87d6b7c784e6
Cobra 陳俊宇 <cobra20621@gmail.com>
2025-05-07 17:05:52 +0800

    fix: relics missing when direct leave game

--- Assets/Scripts/Save/SaveWhenLeaveGame.cs
+++ Assets/Scripts/Save/SaveWhenLeaveGame.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Save
 {
@@ -7,10 +8,23 @@ namespace Save
     /// </summary>
     public class SaveWhenLeaveGame : MonoBehaviour
     {
+        
         private void OnApplicationQuit()
         {
-            SaveManager.Instance.SaveSingleGame();
+            // 如果正在遊戲中(非開頭介面)，需要儲存單局遊戲資料
+            if (!IsMenuGameScene())
+            {
+                SaveManager.Instance.SaveSingleGame();
+            }
+            
             SaveManager.Instance.SavePermanentGame();
+        }
+
+        private bool IsMenuGameScene()
+        {
+            var currentScene = SceneManager.GetActiveScene().name;
+
+            return currentScene == "0- Main Menu";
         }
     }
 }
