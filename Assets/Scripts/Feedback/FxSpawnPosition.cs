befac2162cc8f6efe348ab1a0a99a4451c9bb910
Cobra 陳俊宇 <cobra20621@gmail.com>
2025-05-30 16:44:10 +0800

    refactor: Card Data

--- Assets/Scripts/Feedback/FxSpawnPosition.cs
+++ Assets/Scripts/Feedback/FxSpawnPosition.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using Sirenix.OdinInspector;
 
 namespace Feedback
 {
     [Serializable]
     public enum FxSpawnPosition
     {
-        EachTarget = 0, // 每一個目標
-        Ally = 1, // 玩家
-        ScreenMiddle = 2, // 畫面中間
-        EnemyMiddle = 3, // 敵人中間
+        [LabelText("每一個作用目標")]
+        EachTarget = 0,
+        [LabelText("玩家身上")]
+        Ally = 1, 
+        [LabelText("畫面中間")]
+        ScreenMiddle = 2,
+        [LabelText("敵人中間")]
+        EnemyMiddle = 3, 
     }
 }
